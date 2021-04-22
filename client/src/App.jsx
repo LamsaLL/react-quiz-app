9177199fefe1dfbffffb180bec9add8461597579
LamsaLL <leo.amsallem@gmail.com>
2021-04-22 23:46:51 +0200

    Change root directory of project

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="bg-gray-100 py-6">Salut</div>
+  );
+}
+
+export default App;
--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import logo from './logo.svg';
-
-function App() {
-  return (
-    <div className="bg-gray-100 py-6"></div>
-  );
-}
-
-export default App;
