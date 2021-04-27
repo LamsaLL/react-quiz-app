7e46bfbe99e45ce77e102f2af9fe4025917f6032
LamsaLL <leo.amsallem@gmail.com>
2021-04-27 18:31:20 +0200

    feat: fetch questions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,12 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 
 const App = () => {
-  const [msg, setMsg] = useState('');
+  const [questions, setQuestions] = useState([]);
 
   const handleClick = () => {
-    fetch('/api/question')
+    fetch('/api/questions')
       .then(response => response.json())
-      .then(data => setMsg(data.msg));
+      .then(data => setQuestions(data));
   }
 
   return (
@@ -16,7 +16,11 @@ const App = () => {
       <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> 
         Click me 
       </button>
-      <h1> {msg} </h1>      
+      {
+        questions.map((question) => (
+          <h1>{question.text_question}</h1>
+        ))
+      }     
     </div>
   );
 }
