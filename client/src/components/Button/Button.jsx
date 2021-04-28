7ebebd1affcfae862bbc9e54360798ac71e2cd0e
LamsaLL <leo.amsallem@gmail.com>
2021-04-28 17:18:29 +0200

    feat(Button): Add Button component

---
+++ client/src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({ icon = null, children = null, text = null, className = '', ...rest }) => (
+    <button className={`w-max flex items-center font-semibold rounded-full px-4 py-1 border cursor-pointer
+    focus:outline-none transform transition-transform hover:scale-105 whitespace-nowrap ${className}`}>
+        {text}
+    </button>
+);
+
+export default Button;
+
