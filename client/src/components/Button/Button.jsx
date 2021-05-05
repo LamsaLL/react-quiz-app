9f1da7d926953b1da532a83b1b0d80d9c0ac7fa2
LamsaLL <leo.amsallem@gmail.com>
2021-05-05 18:12:01 +0200

    feat(Button): Added ...rest parameter

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -1,5 +1,5 @@
 const Button = ({ icon = null, children = null, text = null, className = '', ...rest }) => (
-    <button className={`w-max flex items-center font-semibold rounded-full px-4 py-1 border cursor-pointer
+    <button {...rest} className={`w-max flex items-center font-semibold rounded-full px-4 py-1 border cursor-pointer
     focus:outline-none transform transition-transform hover:scale-105 whitespace-nowrap ${className}`}>
         {text}
     </button>
