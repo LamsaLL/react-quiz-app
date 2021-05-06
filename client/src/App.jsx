54b832449330c7d9c85f34cb31d6dd051e0e2615
LamsaLL <leo.amsallem@gmail.com>
2021-05-06 17:23:46 +0200

    feat(App): replay a party

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,62 +1,86 @@
 
-import React, { useState } from 'react';
-import useFetch from './hooks/useFetch.jsx'
+import React, { useReducer, useState } from 'react';
+import useFetch from './hooks/useFetch.jsx';
 
-import Button from './components/Button/Button.jsx'
+import Score from './components/Score/Score.jsx';
+import Questions from './components/Questions/Questions.jsx'
 import Card from './components/Card/Card.jsx';
 import Layout from './components/Layout/Layout.jsx';
+
+const initialState = {
+  currentQuestion: 0,
+  userAnswer: false,
+  score: 0,
+  showScore: false
+};
+
+const reducer = (state, action) => {
+  if (action.type === "RESET") {
+    return initialState;
+  }
+  const result = { ...state };
+  result[action.type] = action.value;
+
+  return result;
+};
+
 
 const App = () => {
   const { data = [], loading, error } = useFetch('/api/randomQuestions', {});
 
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [userAnswer, setUserAnswer] = useState(false);
-  const [score, setScore] = useState(0);
-  const [showScore, setShowScore] = useState(false)
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { currentQuestion, userAnswer, score, showScore } = state;
 
   const text_question = data.length !== 0 ? data[currentQuestion].question_text : "";
+  const answer_question = data.length !== 0 ? data[currentQuestion].answer : "";
 
-  const handleTrueButtonClick = () =>{
-    setUserAnswer(true);
+  const handleTrueButtonClick = () => {
+    dispatch({ type: 'userAnswer', value: true });
   }
 
-  const handleFalseButtonClick = () =>{
-    setUserAnswer(false);    
+  const handleFalseButtonClick = () => {
+    dispatch({ type: 'userAnswer', value: false });
   }
 
   const handleValidateButtonClick = () => {
     const nextQuestion = currentQuestion + 1;
-    
-    if ( userAnswer === data[currentQuestion].answer) {
-      setScore(score + 1);
+
+    if (userAnswer === answer_question) {
+      dispatch({ type: 'score', value: score + 1 });
     }
-    
+
     if (nextQuestion < data.length) {
-      setCurrentQuestion(nextQuestion);
+      dispatch({ type: 'currentQuestion', value: nextQuestion });
     } else {
-      setShowScore(true);
+      dispatch({ type: 'showScore', value: true });
     }
   };
+
+  const handleRestartButtonClick = () => {
+    dispatch({ type: "RESET" });
+  }
 
   return (
     <Layout className="h-screen items-center justify-center bg-blue-200">
       <div className="container">
         <Card className="shadow-2xl">
           {
-            <div className="p-4 text-center text-xl">
-              {error && 'Error!'}
-              {loading && 'Loading...'}
-              {text_question}
-            </div>
+            showScore ?
+              <Score
+                score={score}
+                numberOfQuestions={data.length}
+                handleRestartButtonClick={handleRestartButtonClick}
+              />
+              :
+              <Questions
+                error={error}
+                loading={loading}
+                text_question={text_question}
+                handleTrueButtonClick={handleTrueButtonClick}
+                handleFalseButtonClick={handleFalseButtonClick}
+                handleValidateButtonClick={handleValidateButtonClick}
+              />
           }
-          <div className="flex flex-wrap gap-4 p-4 justify-center">
-            <Button className="focus:bg-green-400" onClick={handleTrueButtonClick} text="Vraie"> </Button>
-            <Button className="focus:bg-red-400" onClick={handleFalseButtonClick} text="Faux"> </Button>
-            <Button className="text-white bg-blue-400" onClick={handleValidateButtonClick} text="Valider" > </Button>
-          </div>
-          <div className=''>
-            {showScore ? <div className='score-section'> Your score is {score}/{data.length} </div> : <></>}
-          </div>
         </Card>
       </div>
     </Layout>
