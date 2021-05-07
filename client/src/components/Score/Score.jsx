7e1e67c6454e437018896cca876b19295c07af1f
LamsaLL <leo.amsallem@gmail.com>
2021-05-07 15:59:01 +0200

    feat(Score): new tailwind style

--- client/src/components/Score/Score.jsx
+++ client/src/components/Score/Score.jsx
@@ -1,9 +1,13 @@
 import Button from '../Button/Button.jsx';
 
 const Score = ({ className = '', score, numberOfQuestions, handleRestartButtonClick }) => (
-    <div className={className}>
-        Your score is {score}/{numberOfQuestions}
-        <Button className="text-white bg-blue-400" onClick={handleRestartButtonClick} text="Rejouer" />
+    <div className={`relative p-2 ${className}`}>
+       <div className="p-16 text-center text-xl">
+           Votre score: <span className="font-bold">{score}/{numberOfQuestions}</span> 
+        </div>
+        <div className="absolute p-2 right-0 bottom-0">
+            <Button className=" text-white bg-blue-400" onClick={handleRestartButtonClick} text="Rejouer" />
+        </div>
     </div>
 );
 
