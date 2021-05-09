c5bbb2d11e9b49c929807433d43b66b2cd04465a
LamsaLL <leo.amsallem@gmail.com>
2021-05-09 15:12:36 +0200

    build(prettier + eslint): set up prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      node: true,
+    },
+    parserOptions: {
+      parser: 'babel-eslint',
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    extends: [],
+    plugins: [],
+    // add your custom rules here
+    rules: {
+     
+    },
+};
+  
